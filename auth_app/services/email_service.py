0cabac44ae206204ef7271454630f97e3f0d4522
LarsThoennes <larsthoennes@gmail.com>
2026-02-18 20:01:41 +0100

    feat(email): implement activation email service with HTML support
    - Added email_service module
    - Implemented send_activation_email function
    - Send multipart email (text + HTML)
    - Render activation email using template
    - Attach activation link with token

---
+++ auth_app/services/email_service.py
@@ -0,0 +1,34 @@
+from django.core.mail import EmailMultiAlternatives
+from django.template.loader import render_to_string
+
+
+def send_activation_email(user, token):
+    activation_link = f"http://localhost:8000/api/activate/?token={token}"
+
+    subject = "Willkommen bei Videoflix"
+
+    # Text-Version (Fallback)
+    text_content = (
+        "Vielen Dank für Ihre Registrierung bei Videoflix!\n\n"
+        "Bitte klicken Sie auf den folgenden Link, um Ihr Konto zu aktivieren:\n"
+        f"{activation_link}"
+    )
+
+    # HTML-Version aus Template
+    html_content = render_to_string(
+        "emails/activation_email.html",
+        {
+            "user": user,
+            "activation_link": activation_link,
+        },
+    )
+
+    msg = EmailMultiAlternatives(
+        subject,
+        text_content,
+        None,
+        [user.email],
+    )
+
+    msg.attach_alternative(html_content, "text/html")
+    msg.send()
