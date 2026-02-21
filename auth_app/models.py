20c2b605582c04f79e4ca2bd574f73c9e23bf566
LarsThoennes <larsthoennes@gmail.com>
2026-02-21 18:10:32 +0100

    refactor(auth): remove UserProfile model

--- auth_app/models.py
+++ auth_app/models.py
@@ -1,6 +1,3 @@
 from django.db import models
 from django.contrib.auth.models import User
 
-class UserProfile(models.Model):
-    user = models.OneToOneField(User, on_delete=models.CASCADE)
-    is_active = models.BooleanField(default=False)
