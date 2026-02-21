fe46deef3f31c11fef195aaf6f10d94fc3fe8726
LarsThoennes <larsthoennes@gmail.com>
2026-02-21 22:36:47 +0100

    feat(auth): register logout route

--- auth_app/api/urls.py
+++ auth_app/api/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-from .views import RegistrationView, ActivateUserProfileView, LoginView
+from .views import RegistrationView, ActivateUserView, LoginView, LogoutView
 
 urlpatterns = [
     path('register/', RegistrationView.as_view(), name='register'),
-    path('activate/<uidb64>/<token>/', ActivateUserProfileView.as_view(), name='activate'),
+    path('activate/<uidb64>/<token>/', ActivateUserView.as_view(), name='activate'),
     path('login/', LoginView.as_view(), name='login'),
+    path('logout/', LogoutView.as_view(), name='logout'),
 ]
