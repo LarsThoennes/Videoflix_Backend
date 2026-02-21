1f03195fa5a8cdb67ffe4f65917111da4a1c0266
LarsThoennes <larsthoennes@gmail.com>
2026-02-21 18:07:17 +0100

    feat(auth): register login route

--- auth_app/api/urls.py
+++ auth_app/api/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from .views import RegistrationView, ActivateUserProfileView
+from .views import RegistrationView, ActivateUserProfileView, LoginView
 
 urlpatterns = [
     path('register/', RegistrationView.as_view(), name='register'),
     path('activate/<uidb64>/<token>/', ActivateUserProfileView.as_view(), name='activate'),
+    path('login/', LoginView.as_view(), name='login'),
 ]
