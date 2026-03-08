071aef9230d4784697d77de7c7f3b29e68cf13b7
LarsThoennes <larsthoennes@gmail.com>
2026-03-08 21:03:09 +0100

    refactor(signals): simplify post_save for video processing
    * Only enqueue  if  exists
    * Removed debug prints and post_delete cleanup (handled elsewhere)

--- video_app/signals.py
+++ video_app/signals.py
@@ -1,22 +1,12 @@
-import os
 import django_rq
 from django.dispatch import receiver
-from django.db.models.signals import post_save, post_delete
+from django.db.models.signals import post_save
 from .models import Video
 from .tasks import convert_video
 
 @receiver(post_save, sender=Video)
 def video_post_save(sender, instance, created, **kwargs):
-    print("Video post save signal triggered")
-    if created:
-        print(f"New video created")
-        queue = django_rq.get_queue('default', autocommit=True)
-        queue.enqueue(convert_video, instance.id)
-
-@receiver(post_delete, sender=Video)
-def video_post_delete(sender, instance, **kwargs):
-    print("Video post delete signal triggered")
-    if instance.original_file:
-        if os.path.isfile(instance.original_file.path):
-            os.remove(instance.original_file.path)
-            print(f"Deleted video file: {instance.original_file.path}")
+    if not instance.original_file:
+        return
+    queue = django_rq.get_queue('default')
+    queue.enqueue(convert_video, instance.id)
